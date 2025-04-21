4e9c2c13a2ba4cd6e33ad1599378ceb11d9cb86d
Antonio Mark <antonio.mark@outook.de>
2025-04-21 11:15:53 +0200

    Initial commit from Spring Initializer

---
+++ src/main/java/fairsource/demo/taskmanagement/TaskManagementApplication.java
@@ -0,0 +1,13 @@
+package fairsource.demo.taskmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementApplication.class, args);
+    }
+
+}
