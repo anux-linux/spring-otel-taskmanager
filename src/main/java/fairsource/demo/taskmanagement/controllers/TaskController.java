c8bd297ad7ae606bf1b49cab67594bf23195c0fe
Antonio Mark <antonio.mark@outook.de>
2025-04-22 15:58:35 +0200

    Working docker Version

--- src/main/java/fairsource/demo/taskmanagement/controllers/TaskController.java
+++ src/main/java/fairsource/demo/taskmanagement/controllers/TaskController.java
@@ -1,8 +1,9 @@
 package fairsource.demo.taskmanagement.controllers;
 
 import fairsource.demo.taskmanagement.models.Task;
-import fairsource.demo.taskmanagement.repositories.TaskRepository;
 import fairsource.demo.taskmanagement.services.impl.TaskServiceImpl;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,10 +15,10 @@ import java.util.List;
 @RequestMapping("/")
 public class TaskController {
 
+    Logger logger = LoggerFactory.getLogger(TaskController.class);
+
     @Autowired
     private final TaskServiceImpl taskService;
-    @Autowired
-    private TaskRepository taskRepository;
 
     public TaskController(TaskServiceImpl taskService) {
         this.taskService = taskService;
@@ -25,76 +26,71 @@ public class TaskController {
 
     @GetMapping
     public String getAllTasks(Model model) {
+        logger.info("Getting all tasks");
         List<Task> tasks = taskService.findAllTasks();
         model.addAttribute("tasks", tasks);
         return "list";
     }
 
     @GetMapping("/{id}")
-    public String getTaskById(@PathVariable Long id, Model model) throws Throwable {
+    public String getTaskById(@PathVariable Long id, Model model) {
+        logger.info("Getting task with the ID {}", id);
         Task task = (Task) taskService.findTaskById(id);
         model.addAttribute("task", task);
-        return "details";
+        return "edit";
     }
 
     @GetMapping("/tasks/create")
     public String showCreateForm(Model model) {
+        logger.info("Show create form for task");
         model.addAttribute("task", new Task());
         return "create";
     }
 
     @PostMapping("/tasks/markOpen")
     public String openTask(@RequestParam Long id) {
-        Task task = taskRepository.findById(id).orElseThrow();
+        logger.info("Set task status to open");
+        Task task = taskService.findTaskById(id);
         task.setDone(false);
-        taskRepository.save(task);
+        taskService.saveTask(task);
         return "redirect:/";
 
     }
 
     @PostMapping("/tasks/markDone")
     public String closeTask(@RequestParam Long id) {
-        Task task = taskRepository.findById(id).orElseThrow();
+        logger.info("Set task status to done");
+        Task task = taskService.findTaskById(id);
         task.setDone(true);
-        taskRepository.save(task);
+        taskService.saveTask(task);
         return "redirect:/";
 
     }
-
-    //@PostMapping
-    //public String createTask(@ModelAttribute("task") Task task) {
-    //    taskService.saveTask(task);
-    //    return "redirect:/";
-    //}
 
     @PostMapping
     public String createTask(@ModelAttribute Task task) {
-        taskService.saveTask(task);
-        // Redirect to the task details page for the newly created task
-        return "redirect:/" + task.getId(); // Assuming the task details page URL includes the task ID
-    }
-
-
-    @GetMapping("/{id}/edit")
-    public String showEditForm(@PathVariable Long id, Model model) throws Throwable {
-        Task task = (Task) taskService.findTaskById(id);
-        model.addAttribute("task", task);
-        return "edit";
-    }
-
-    @PostMapping("/{id}")
-    public String updateTask(@PathVariable Long id, @ModelAttribute("task") Task taskDetails) throws Throwable {
-
-        Task task = (Task) taskService.findTaskById(id);
-
-        task.setName(taskDetails.getName());
-        task.setDone(taskDetails.isDone());
+        logger.info("Creating new task");
         taskService.saveTask(task);
         return "redirect:/";
     }
 
-    @PostMapping("/{id}/delete")
+
+    @PostMapping("/{id}")
+    public String updateTask(@PathVariable Long id, @ModelAttribute("task") Task taskDetails) {
+
+        logger.info("Updating task with with ID {} ", id);
+        Task task = taskService.findTaskById(id);
+
+        task.setName(taskDetails.getName());
+        task.setDone(taskDetails.isDone());
+        task.setPriority(taskDetails.getPriority());
+        taskService.saveTask(task);
+        return "redirect:/";
+    }
+
+    @DeleteMapping("/{id}")
     public String deleteTask(@PathVariable Long id) {
+        logger.info("Deleting task with with ID {} ", id);
         taskService.deleteTask(id);
         return "redirect:/";
     }
--- src/main/java/fairsource/demo/taskmanagement/services/impl/TaskServiceImpl.java
+++ src/main/java/fairsource/demo/taskmanagement/services/impl/TaskServiceImpl.java
@@ -1,20 +1,13 @@
 package fairsource.demo.taskmanagement.services.impl;
 
-import fairsource.demo.taskmanagement.services.TaskService;
 import fairsource.demo.taskmanagement.models.Task;
 import fairsource.demo.taskmanagement.repositories.TaskRepository;
-
-
+import fairsource.demo.taskmanagement.services.TaskService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.nio.file.AccessDeniedException;
 import java.util.List;
-import java.util.NoSuchElementException;
 
 @Service
 public class TaskServiceImpl implements TaskService {
@@ -41,6 +34,6 @@ public class TaskServiceImpl implements TaskService {
     @Override
     public void deleteTask(Long id) {
         Task task = taskRepository.findById(id).orElseThrow();
-            taskRepository.deleteById(id);
+        taskRepository.deleteById(id);
     }
 }
